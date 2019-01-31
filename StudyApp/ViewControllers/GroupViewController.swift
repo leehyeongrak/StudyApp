69e2da8c34653f1d5102bcf85da5e914242c2941
RAK <>
2019-01-31 15:51:02 +0900

    pod init 및 firebase pod install

--- StudyApp/ViewControllers/GroupViewController.swift
+++ StudyApp/ViewControllers/GroupViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class GroupViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
