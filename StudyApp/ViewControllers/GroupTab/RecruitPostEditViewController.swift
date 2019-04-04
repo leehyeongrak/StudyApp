faf1c1a40b7f212a5f139b0b40ae1ef4e7cd93f7
RAK <leehrak@gmail.com>
2019-04-04 14:11:31 +0900

    testCommit

---
+++ StudyApp/ViewControllers/GroupTab/RecruitPostEditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecruitPostEditViewController.swift
+//  StudyApp
+//
+//  Created by RAK on 15/02/2019.
+//  Copyright © 2019 RAK. All rights reserved.
+//
+
+import UIKit
+
+class RecruitPostEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
